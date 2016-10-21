7739917df9ce503ba53e99d9e1bcfd12734ee814
00steve <stevemkrenz@gmail.com>
2016-10-21 16:13:37 -0500

    Removed engine system files, now that they are in an independent project, they don't need to be in this one anymore. This project now only exists for testing those system files.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
-#include "engine/engine.h"
+#include <engine/engine.h>
 
 
-int main()
+int main(int argc, char* argv[])
 {
+
+
     Engine e;
     return 0;
 }
